c1869b68c338ef88d7636909dc4ec0863c938f44
idk <idk@idk.local>
2019-11-22 20:10:15 -0500

    first attempt

--- main.sh
+++ main.sh
@@ -8,6 +8,6 @@ tar cf - $(pwd)/mnt | (cd $(pwd)/tmp; tar xfp -)
 
 wget -c $(pwd)/tmp/EFI/boot https://github.com/hirotakaster/baytail-bootia32.efi/raw/master/bootia32.efi
 
-mkisofs -o update-$1 -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V "$1 Customized" $(pwd)/tmp
+mkisofs -o update-$1 -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V "32Bit UEFI .iso" $(pwd)/tmp
 
 sudo umount $(pwd)/mnt
