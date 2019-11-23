4b5922c9fc283c0000b1547c5c06ba67608ef90c
idk <idk@idk.local>
2019-11-22 20:28:31 -0500

    first attempt

--- main.sh
+++ main.sh
@@ -8,7 +8,8 @@ sudo mount -t iso9660 -o loop $1 $wd/mnt
 cd $wd/mnt
 tar cf - . | (cd $wd/tmp; tar xfp -)
 
-sudo wget -c $wd/tmp/EFI/boot/bootia32.efi https://github.com/hirotakaster/baytail-bootia32.efi/raw/master/bootia32.efi
+wget -c $wd/bootia32.efi https://github.com/hirotakaster/baytail-bootia32.efi/raw/master/bootia32.efi
+sudo cp $wd/bootia32.efi $wd/tmp/EFI/boot/bootia32.efi
 
 cd $wd/tmp
 mkisofs -o update-$1 -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V "32Bit UEFI .iso"
